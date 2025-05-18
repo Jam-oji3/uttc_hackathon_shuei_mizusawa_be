82f1ce532e88ba6c38a496622b1f96b7b7f76f6e
Jam-oji3 <shuei3150@gmail.com>
2025-05-18 18:21:35 +0900

    UserNotFoundエラーを明示的に定義

--- hackathon/dao/user_dao.go
+++ hackathon/dao/user_dao.go
@@ -2,6 +2,7 @@ package dao
 
 import (
 	"database/sql"
+	"errors"
 	"hackathon/model"
 )
 
@@ -14,12 +15,13 @@ func (dao *UserDAO) FindById(id string) (*model.User, error) {
 	SELECT id, username, display_name, email, bio, icon_url, created_at, updated_at
 	FROM user
 	WHERE id = ?`, id)
-	if err := row.Err(); err != nil {
-		return nil, err
-	}
 
 	var u model.User
-	if err := row.Scan(&u.Id, &u.UserName, &u.DisplayName, &u.Email, &u.Bio, &u.IconURL, &u.CreatedAt, &u.UpdatedAt); err != nil {
+	err := row.Scan(&u.Id, &u.UserName, &u.DisplayName, &u.Email, &u.Bio, &u.IconURL, &u.CreatedAt, &u.UpdatedAt)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, model.ErrUserNotFound
+	}
+	if err != nil {
 		return nil, err
 	}
 	return &u, nil
@@ -30,12 +32,13 @@ func (dao *UserDAO) FindByUserName(userName string) (*model.User, error) {
 	SELECT id, username, display_name, email, bio, icon_url, created_at, updated_at
 	FROM user
 	WHERE username = ?`, userName)
-	if err := row.Err(); err != nil {
-		return nil, err
-	}
 
 	var u model.User
-	if err := row.Scan(&u.Id, &u.UserName, &u.DisplayName, &u.Email, &u.Bio, &u.IconURL, &u.CreatedAt, &u.UpdatedAt); err != nil {
+	err := row.Scan(&u.Id, &u.UserName, &u.DisplayName, &u.Email, &u.Bio, &u.IconURL, &u.CreatedAt, &u.UpdatedAt)
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, model.ErrUserNotFound
+	}
+	if err != nil {
 		return nil, err
 	}
 	return &u, nil
@@ -51,17 +54,38 @@ func (dao *UserDAO) Insert(tx *sql.Tx, user *model.User) error {
 }
 
 func (dao *UserDAO) Update(tx *sql.Tx, user *model.User) error {
-	_, err := tx.Exec(`
-	UPDATE user 
-	SET display_name = ?, bio = ?, icon_url = ?, updated_at = ?
-	WHERE id = ?`,
+	res, err := tx.Exec(`
+		UPDATE user 
+		SET display_name = ?, bio = ?, icon_url = ?, updated_at = ?
+		WHERE id = ?`,
 		user.DisplayName, user.Bio, user.IconURL, user.UpdatedAt, user.Id)
-	return err
+	if err != nil {
+		return err
+	}
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows == 0 {
+		return model.ErrUserNotFound
+	}
+	return nil
 }
 
 func (dao *UserDAO) Delete(tx *sql.Tx, id string) error {
-	_, err := tx.Exec(`
+	res, err := tx.Exec(`
 	DELETE FROM user 
 	WHERE id = ?`, id)
-	return err
+	if err != nil {
+		return err
+	}
+
+	rows, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rows == 0 {
+		return model.ErrUserNotFound
+	}
+	return nil
 }
