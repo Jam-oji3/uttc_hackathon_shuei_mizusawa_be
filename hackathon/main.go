c6c35f2208dc592690dd619af8db58df704b5edd
Jam-oji3 <shuei3150@gmail.com>
2025-06-13 23:12:03 +0900

    feat: 認証用エントリポイントを追加

--- hackathon/main.go
+++ hackathon/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"context"
 	"hackathon/controller"
+	"hackathon/infra/firebase"
 	"hackathon/infra/mysql"
 	"hackathon/usecase"
 	"log"
@@ -12,26 +13,37 @@ import (
 func main() {
 	db, err := mysql.InitDB()
 	if err != nil {
-		fmt.Printf("fail: InitDB(), %v\n", err)
+
+		log.Fatalf("fail: InitDB(), %v\n", err)
 	}
 	defer db.Close()
 	mysql.CloseDBWithSysCall(db)
 
+	ctx := context.Background()
+
+	firebaseAuthRepo, err := firebase.NewFirebaseAuthRepository(ctx)
+	if err != nil {
+		log.Fatalf("fail: InitFirebaseAuthRepo(), %v\n", err)
+	}
+
 	userRepo := mysql.NewUserRepository()
 	txExecutor := mysql.NewTxExecutor()
+
+	AuthUC := usecase.NewAuthUserUseCase(firebaseAuthRepo, userRepo, db)
 	registerUC := usecase.NewUserRegisterUseCase(txExecutor, userRepo, db)
 
-	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
-		switch r.Method {
-		case http.MethodPost:
-			controller := &controller.UserRegisterController{UseCase: registerUC}
-			controller.ServeHTTP(w, r)
-		default:
-			w.WriteHeader(http.StatusMethodNotAllowed)
-		}
+	AuthC := controller.NewAuthUserController(AuthUC)
+	UserRegisterC := controller.NewUserRegisterController(registerUC)
+
+	http.HandleFunc("/auth", func(w http.ResponseWriter, r *http.Request) {
+		AuthC.ServeHTTP(w, r)
 	})
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		UserRegisterC.ServeHTTP(w, r)
+	})
+
 	log.Println("Listening on :8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
-		fmt.Printf("fail: ListenAndServe(), %v\n", err)
+		log.Fatalf("fail: ListenAndServe(), %v\n", err)
 	}
 }
